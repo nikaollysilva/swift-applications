19bdcf03d07da14c80facb6134cdadd4b394a86e
Nikolly Silva <53615322+nikaollysilva@users.noreply.github.com>
2020-05-28 14:47:17 -0300

    Add session 2 completed

---
+++ Session02/Aplicativo - EuSouRico/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Nikolly Santos Da Silva on 03/04/20.
+//  Copyright © 2020 Nikolly Santos Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Session02/teste/teste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teste
+//
+//  Created by Nikolly Santos Da Silva on 09/04/20.
+//  Copyright © 2020 Nikolly Santos Da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
